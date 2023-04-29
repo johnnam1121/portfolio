1f94d6c7bb4655ed5fd80f64d4ccab91b25698d9
johnnam1121 <johnnam93@gmail.com>
2023-04-29 13:44:11 -0500

    added sliding feature.. had to use framer motion

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,23 +1,39 @@
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
 import { styled } from '@mui/material/styles';
-import React from 'react'
-import SlideTransition from './SlideTransition';
+import { motion } from 'framer-motion';
+import projects from './ProjectsList';
 
 const styles = {
   textDecoration: 'none',
-  '&hover': {
-    
-  }
+  // '&hover': {
+
+  // }
 };
 
 const StyledLink = styled('a')(() => ({
   ...styles
-}))
+}));
 
-function Projects() {
+const transition = {
+  type: 'easeIn',
+  duration: 0.5
+};
+
+const variants = {
+  hidden: {
+    opacity: 0,
+    x: 100
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition
+  }
+};
+
+export default function Projects() {
   return (
     <Container maxWidth={false}>
       <Grid container spacing={2}>
@@ -29,45 +45,28 @@ function Projects() {
         <Grid item md={2} />
         <Grid item md={2} />
         <Grid item xs={12} md={8} sx={{ textAlign: 'center', mb: '5vh' }}>
-          <StyledLink href="google.com" target='_blank' rel='noopener noreferrer'>
-            <Typography variant="h1">
-              One Shot Pocha
-            </Typography>
-          </StyledLink>
-          <StyledLink href="google.com" target='_blank' rel='noopener noreferrer'>
-            <Typography variant="h1">
-              Live Chat App
-            </Typography>
-          </StyledLink>
-          <StyledLink href="google.com" target='_blank' rel='noopener noreferrer'>
-            <Typography variant="h1">
-              Youtube Video Player Clone
-            </Typography>
-          </StyledLink>
-          <StyledLink href="google.com" target='_blank' rel='noopener noreferrer'>
-            <Typography variant="h1">
-              Python Job Post Scraper
-            </Typography>
-          </StyledLink>
-          <StyledLink href="google.com" target='_blank' rel='noopener noreferrer'>
-            <Typography variant="h1">
-              SC2 Unit Counter
-            </Typography>
-          </StyledLink>
-          <StyledLink href="google.com" target='_blank' rel='noopener noreferrer'>
-            <Typography variant="h1">
-              Little Lemon
-            </Typography>
-          </StyledLink>
-          <StyledLink href="google.com" target='_blank' rel='noopener noreferrer'>
-            <Typography variant="h1">
-              SIS Newspaper Wordpress
-            </Typography>
-          </StyledLink>
+          <motion.div
+            initial='hidden'
+            animate='visible'
+            variants={{
+              visible: {
+                transition: {
+                  delayChildren: 0.5,
+                  staggerChildren: 0.2
+                }
+              }
+            }}
+          >
+            {projects.map((item, index) => (
+              <motion.div variants={variants} key={index}>
+                <StyledLink href={item.liveLink} target='_blank' rel='noopener noreferrer'>
+                  <Typography variant='h1' sx={{ mt: '3vh' }}>{item.project}</Typography>
+                </StyledLink>
+              </motion.div>
+            ))}
+          </motion.div>
         </Grid>
       </Grid>
     </Container>
   )
 }
-
-export default Projects
---
+++ src/components/ProjectsList.tsx
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    project: 'One Shot Pocha',
+    liveLink: 'https://google.com'
+  },
+  {
+    project: 'Live Chat App',
+    liveLink: 'https://google.com'
+  },
+  {
+    project: 'Youtube Video Player Clone',
+    liveLink: 'https://google.com'
+  },
+  {
+    project: 'Python Job Post Scraper',
+    liveLink: 'https://google.com'
+  },
+  {
+    project: 'SC2 Unit Counter',
+    liveLink: 'https://google.com'
+  },
+  {
+    project: 'Little Lemon',
+    liveLink: 'https://google.com'
+  },
+  {
+    project: 'SIS Newspaper Wordpress',
+    liveLink: 'https://google.com'
+  }
+];
+
+export default projects
